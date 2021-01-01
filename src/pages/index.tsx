8663a1e3de5f7a36e543b0b63e4d9080309a109b
Ícaro Ferreira de SOuza <icaroferreira.souza@gmail.com>
2021-01-01 18:39:32 -0300

    WIP - chamada a api com Cliente Side Fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
 import { Titlte } from "../styles/pages/Home";
 
+interface IProduct {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>(
+    []
+  );
+
+  useEffect(() => {
+    fetch("http://localhost:3333/recommended").then((response) => {
+      response.json().then((data) => {
+        setRecommendedProducts(data);
+      });
+    });
+  }, []);
+
   return (
     <div>
-      <Titlte>Hello Ícaro</Titlte>
+      <section>
+        <Titlte>Products</Titlte>
+
+        <ul>
+          {recommendedProducts.map((recommendedProduct) => {
+            return (
+              <li key={recommendedProduct.id}>{recommendedProduct.title}</li>
+            );
+          })}
+        </ul>
+      </section>
     </div>
   );
 }
