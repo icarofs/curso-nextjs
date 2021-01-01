2b8e0990cb3959f573dc8d79ae03d01a49a1af83
Ícaro Ferreira de SOuza <icaroferreira.souza@gmail.com>
2021-01-01 18:23:03 -0300

    WIP - config inicial da estrutura de pastas e adicionado styled-components

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Titlte } from "../styles/pages/Home";
+
+export default function Home() {
+  return (
+    <div>
+      <Titlte>Hello Ícaro</Titlte>
+    </div>
+  );
+}
---
+++ src/styles/pages/Home.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const Titlte = styled.h1`
+  color: #8257e4;
+`
